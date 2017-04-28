a639a93c0b486a316c3b116ee6b48f0d47faa17f
Shiron manuelpillai <shironm1993@gmail.com>
2017-04-28 17:33:07 +0100

    smoothstate, jquery older version

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 var scss = require('./styles/style.scss');
 import "jQuery";
+import "./vendor/smoothstate/jquery.smoothState.min.js";
 
-//nav menu
+$(function() {
 
+
+function fn(){
+    
+    //so font awesome can be recognised
+    var scss = require('./styles/style.scss');
+
+    //nav menu
 $('.menu-icon').on('click', function (){
     $(this).toggleClass('hamburger cross');
     $('.overlay').toggleClass('overlay--show');
@@ -108,4 +116,43 @@ $('#message').on('keyup focus', function (e) {
     }
 })
 
+}
+// end of fn
 
+fn();
+
+
+
+//smoothstate
+
+var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    },
+      onAfter: function($container, $newContent) {
+      fn();
+  }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+
+});

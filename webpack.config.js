d7e3f2c77805136052de1e2403c52d841c990408
Shiron manuelpillai <shironm1993@gmail.com>
2017-04-30 23:35:51 +0100

    variables and images url loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
        test: /\.scss$/,
        use: ExtractTextPlugin.extract({
          fallback: 'style-loader',
-         use: ['css-loader?sourceMap','postcss-loader', 'sass-loader?sourceMap']
+         use: ['css-loader?url=false','postcss-loader', 'sass-loader?sourceMap']
        })
      }
    ]

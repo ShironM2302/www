60e70f665ccfb5dc72d9d04f917a954567be43f7
Shiron manuelpillai <shironm1993@gmail.com>
2017-05-04 00:05:10 +0100

    GSAp and hero shapes

--- src/script.js
+++ src/script.js
@@ -11,6 +11,208 @@ $(function() {
 
 
 function fn(){
+
+    var master = new TimelineMax()
+        // .add(mainBanner())
+        // .add(leftBanner())
+        // .add(rightBanner())
+        .add(centerBanner());
+
+function mainBanner(){
+        var main = new TimelineMax({
+        // repeat:-1
+    });
+
+    main
+    .to('.hero-title',1.5,{
+        ease: Power0.easeNone,
+        y:10,
+        autoAlpha:1
+    });
+
+    return main;
+
+}
+
+function leftBanner(){
+
+    var left = new TimelineMax({
+        // repeat:-1
+    });
+
+    left.set('.f-ball--left',{
+        left:'15%',
+        top:'15%',
+        autoAlpha:0
+    })
+    .to('.f-ball--left',1,{
+        autoAlpha:1
+    })
+    .to('.f-ball--left',1,{
+        top:'90%',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--left',0.15,{
+        scaleX:1.5,
+        scaleY:0.6
+    })
+    .to('.f-ball--left',.5,{
+        scaleX:1,
+        scaleY:1,
+        top:'0%',
+        y:0,
+        left:0,
+        autoAlpha:0.35,
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--left',1.2,{
+        ease: Power0.easeNone,
+        borderRadius:'0',
+        width:'100%',
+        height:'10vh'                
+    })
+    .set('.f-ball--left',{
+        borderRadius:'0',
+        width:'100%',
+        height:'10vh'                
+    })
+    .to('.trait-one',1.2,{
+        ease: Power0.easeNone,
+        autoAlpha:1  
+    });
+
+    return left;
+}
+   
+function rightBanner(){
+
+    var heightBox = $('.hero-info').height();
+    var widthBox = $('.hero-info').width();
+    var leftBox = $('.hero-info').position().left;
+    
+    
+    var right = new TimelineMax({
+        // repeat:-1
+    });
+
+    right.set('.f-ball--right',{
+        left:'85%',
+        top:'15%',
+        autoAlpha:0
+    })
+    .to('.f-ball--right',1,{
+        autoAlpha:1
+    })
+    .to('.f-ball--right',1,{
+        top:'90%',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--right',0.15,{
+        scaleX:1.5,
+        scaleY:0.6
+    })
+    .to('.f-ball--right',1,{
+        scaleX:1,
+        scaleY:1,
+        top:'30%',
+        left:'50%',
+        autoAlpha:0.35,
+        // x:'-12.5rem',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--right',.1,{
+        clearProps:'left',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--right',1,{
+        className: 'f-box',
+        borderRadius:0,
+        ease: Circ.easeOut
+    })
+    .to('.dash-one',1.2,{
+        ease: Power0.easeNone,
+        autoAlpha:1  
+    })
+    .to('.trait-two',1.2,{
+        ease: Power0.easeNone,
+        autoAlpha:1  
+    });
+
+    return right;
+
+}
+
+function centerBanner(){
+
+    var center = new TimelineMax({
+        // repeat:-1
+    });
+
+    center.set('.f-ball--center',{
+        left:'50%',
+        xPercent:-50,
+        top:'15%',
+        autoAlpha:0
+    })
+    .to('.f-ball--center',1,{
+        autoAlpha:1
+    })
+    .to('.f-ball--center',1,{
+        top:'90%',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--center',0.2,{
+        scaleX:1.5,
+        scaleY:0.6
+    })
+    .to('.f-ball--center',.2,{
+        scaleX:1,
+        scaleY:1,
+        autoAlpha:0.35,
+        ease:Circ.easeOut
+    })
+    // .to('.f-ball--center',.2,{
+    //     top:'85%',
+    //     ease:Circ.easeOut
+    // })
+    // .to('.f-ball--center',.2,{
+    //     top:'90%',
+    //     ease:Circ.easeOut
+    // })
+    .to('.f-ball--center',.1,{
+        // clearProps:'transform',
+        ease:Circ.easeOut
+    })
+    .to('.f-ball--center',.5,{
+        className: 'f-circle',
+        ease: Circ.easeOut
+    })
+    .to('.arrow-block-hero',.25,{
+        y:-10,
+    })
+    .to('.arrow-block-hero',.25,{
+        ease: Power0.easeNone,
+        y:0,
+        autoAlpha:1
+    })
+    .to('.dash-two',1.2,{
+        ease: Power0.easeNone,
+        autoAlpha:1  
+    })
+    .to('.trait-three',1.2,{
+        ease: Power0.easeNone,
+        autoAlpha:1  
+    });
+
+
+    return center;
+
+}
+   
+
+
+
+
     
     //so font awesome can be recognised
 var scss = require('./styles/style.scss');
